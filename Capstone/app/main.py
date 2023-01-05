70ce3d5674bf29270b101108f43bc0b9d9ca6b4f
Parthvi Kulkarni <parthvikulkarni@Parthvis-MacBook-Pro.local>
2023-01-05 12:14:49 -0700

    Found kicking frequency

--- Capstone/app/main.py
+++ Capstone/app/main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy import signal
+import datetime
 
 # Import csv
 df = pd.read_csv('/Users/parthvikulkarni/Capstone-1/Capstone/app/data/kick_test2.csv')
@@ -24,7 +25,7 @@ ax.set_title('Raw Gyroscope Data in the Z direction')
 ax.set_xlabel('Time')
 ax.set_ylabel('Angular Velocity in the Z direction')
 ax.plot(time, raw_gyr_z, 'b')
-plt.show() 
+#plt.show() 
 
 # Denoise the data by applying an IIR Filter
 b, a = signal.butter(3, 0.05)
@@ -35,25 +36,34 @@ signal_gyr_z = signal.filtfilt(b, a, raw_gyr_z) # apply the filter
 
 # Plot of IIR Filtered Data
 plt.plot(time, signal_gyr_z, 'b')
-plt.show()
+#plt.show()
 
 # TODO: Apply Butterworth Filter to Data
 
+# Peak detection during active periods
+numpeaks = signal.find_peaks(signal_gyr_z) # Gives the indices of the peaks
+firstpeak = numpeaks[0][0]
+lastpeak = numpeaks[0][-1]
+t_first = datetime.datetime.strptime(time[firstpeak], '%H:%M:%S.%f')
+t_last = datetime.datetime.strptime(time[lastpeak], '%H:%M:%S.%f')
+delta = t_last - t_first
+total_kicks = len(numpeaks[0])
+kicking_frequency = total_kicks / delta.total_seconds()
+print('Kicking frequency: ' + str(kicking_frequency) + ' per second')
+
 # Slope detection :  Look for places where the second derivative (der2) is larger (at least half of the signal's max size)
 # Peak detection :  Look at the gaps in between indices to identify where each peak begins and ends to find the midpoint
-window = 21
-der2 = signal.savgol_filter(signal_gyr_z, window_length=window, polyorder=2, deriv=2)
-print(der2)
-max_der2 = np.max(np.abs(der2))
-print(max_der2)
-large = np.where(np.abs(der2) > max_der2/2)[0]
-print(large)
-gaps = np.diff(large) > window
-print(gaps)
-begins = np.insert(large[1:][gaps], 0, large[0])
-print(begins)
-ends = np.append(large[:-1][gaps], large[-1])
-print(ends)
-peaks = ((begins+ends)/2).astype(np.int) 
-
-# TODO: Zero-crossing
+# window = 21
+# der2 = signal.savgol_filter(signal_gyr_z, window_length=window, polyorder=2, deriv=2)
+# print(der2)
+# max_der2 = np.max(np.abs(der2))
+# print(max_der2)
+# large = np.where(np.abs(der2) > max_der2/2)[0]
+# print(large)
+# gaps = np.diff(large) > window
+# print(gaps)
+# begins = np.insert(large[1:][gaps], 0, large[0])
+# print(begins)
+# ends = np.append(large[:-1][gaps], large[-1])
+# print(ends)
+# peaks = ((begins+ends)/2).astype(np.int) 
