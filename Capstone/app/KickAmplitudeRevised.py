d9ec00f5ff4038123fc4c119321729d2ac4f6871
JadeLvn <levine.jade@gmail.com>
2023-03-12 20:02:49 -0700

    Update for Kick Amp

--- Capstone/app/KickAmplitudeRevised.py
+++ Capstone/app/KickAmplitudeRevised.py
@@ -1,20 +1,16 @@
-from shutil import which
-
 import matplotlib.pyplot as plt
 import numpy
 import numpy as np
 import pandas as pd
-import scipy
 from scipy import signal
-import datetime
 import math
-from ahrs.filters import Madgwick
 from pyquaternion import Quaternion
+from vqf import VQF
 
 # Import csv
 from Capstone.app.IFFT import fourierFilt
 
-df = pd.read_csv("C:/Users/jade-/OneDrive/Documents/UBC 2022-23/BMEG 457/Testing/February 2023/Feb22.csv")
+df = pd.read_csv("C:/Users/jade-/OneDrive/Documents/UBC 2022-23/BMEG 457/Testing/February 2023/feb22.csv")
 
 # Retrieve raw data files
 raw_acc_x = df[df.columns[0]]
@@ -29,38 +25,33 @@ time = df[df.columns[7]]
 seconds = []
 
 for i in range(0, length):
-    seconds.append(time[i]/math.pow(10, 6) - time[0]/math.pow(10, 6))
+    seconds.append(time[i] / math.pow(10, 6) - time[0] / math.pow(10, 6))
 
-seconds.append(seconds[-2]+seconds[-2]-seconds[-3])
+seconds.append(seconds[-2] + seconds[-2] - seconds[-3])
 
 raw_A = np.array([raw_acc_x, raw_acc_y, raw_acc_z])
-raw_G = np.array([raw_gyr_x*math.pi/180, raw_gyr_y*math.pi/180, raw_gyr_z*math.pi/180])
+raw_G = np.array([raw_gyr_x, raw_gyr_y, raw_gyr_z * math.pi / 180])
 
-raw_A = raw_A.transpose()
-raw_G = raw_G.transpose()
+raw_A = np.ascontiguousarray(raw_A.transpose())
+raw_G = np.ascontiguousarray(raw_G.transpose())
 
-madgwick_init = Madgwick()
-Q_init = np.tile([1., 0., 0., 0.], (length,1))
-
-for t in range(1, length):
-    Q_init[t] = madgwick_init.updateIMU(Q_init[t - 1], gyr=raw_G[t], acc=raw_A[t])
-
-print(Q_init.shape)
+vqf = VQF(1 / 88)
+out = vqf.updateBatch(raw_G, raw_A)
+Q_init = out['quat6D']
 
 Quat = []
 distRad = []
 distDeg = []
-
 distRad.append(0)
 
 for i in range(0, len(Q_init)):
     Quat.append(Quaternion(Q_init[i]))
 
-for i in range(1, len(Q_init)):
-    distRad.append(Quaternion.distance(Quat[i-1], Quat[i]))
+for i in range(1, len(Q_init) - 1):
+    distRad.append(2 * math.acos(np.real(Quat[i - 1] * Quat[i].conjugate)))
 
-for i in range(0, len(distRad)):
-    distDeg.append(distRad[i]*180/math.pi)
+for i in range(0, len(distRad) - 1):
+    distDeg.append(distRad[i] * 180 / math.pi)
 
 plt.plot(distDeg)
 plt.title('Quaternion angle over time')
@@ -68,24 +59,23 @@ plt.xlabel('time (s)')
 plt.ylabel('angle (deg)')
 plt.show()
 
-b,a = signal.butter(4,0.1,'lowpass')
+b, a = signal.butter(4, 0.1, 'lowpass')
 
-window_size = 88
-numbers_series = pd.Series(signal.filtfilt(b,a,distDeg))
+window_size = 100
+numbers_series = pd.Series(signal.filtfilt(b, a, distDeg))
 windows = numbers_series.rolling(window_size)
 moving_averages = windows.mean()
 moving_averages = np.array(moving_averages.tolist())
 
-thresh = 0.5
-greater = np.where(moving_averages>thresh, 1, moving_averages)
-greater[greater <= thresh] = 0
-greater[greater == numpy.nan] = 0
+thresh = 1.1
+greater = np.where(moving_averages > thresh, 1, moving_averages)
+greater[greater != 1] = 0
 first = np.where(greater == 1)[0][0] - 100
 last = np.where(greater == 1)[0][-1] + 100
 print(first)
 print(last)
 
-length = last-first
+length = last - first
 
 fft_acc_x = fourierFilt(np.array(raw_acc_x[first:last]))
 fft_acc_y = fourierFilt(np.array(raw_acc_y[first:last]))
@@ -99,21 +89,17 @@ acc_z = np.zeros(len(acc_x))
 gyr_x = np.zeros(len(acc_x))
 gyr_y = np.zeros(len(acc_x))
 
-seconds = seconds[first+1:last]
+seconds = seconds[first + 1:last]
 
 A = np.array([acc_x, acc_y, acc_z])
-G = np.array([gyr_x*math.pi/180, gyr_y*math.pi/180, gyr_z*math.pi/180])
+G = np.array([gyr_x * math.pi / 180, gyr_y * math.pi / 180, gyr_z * math.pi / 180])
 
-A = A.transpose()
-G = G.transpose()
+A = np.ascontiguousarray(A.transpose())
+G = np.ascontiguousarray(G.transpose())
 
-madgwick = Madgwick()
-Q = np.tile([1., 0., 0., 0.], (length,1))
-
-for t in range(1, length):
-    Q[t] = madgwick.updateIMU(Q[t - 1], gyr=G[t], acc=A[t])
-
-print(Q.shape)
+vqfF = VQF(1 / 88)
+out = vqfF.updateBatch(G, A)
+Q = out['quat6D']
 
 Quat = []
 distRad = []
@@ -123,13 +109,72 @@ for i in range(0, len(Q)):
     Quat.append(Quaternion(Q[i]))
 
 for i in range(1, len(Q)):
-    distRad.append(Quaternion.distance(Quat[i-1], Quat[i]))
+    distRad.append(2 * math.acos(np.real(Quat[i - 1] * Quat[i].conjugate)))
 
 for i in range(0, len(distRad)):
-    distDeg.append(distRad[i]*180/math.pi)
+    distDeg.append(distRad[i] * 180 / math.pi)
 
 plt.plot(seconds, distDeg)
 plt.title('Quaternion angle over time')
 plt.xlabel('time (s)')
 plt.ylabel('angle (deg)')
+plt.show()
+
+diffGyr = np.diff(gyr_z > 0)
+zeroCross = []
+
+for i in range(0, len(diffGyr) - 1):
+    if diffGyr[i]:
+        zeroCross.append(i)
+
+print(zeroCross)
+print(len(zeroCross))
+plt.plot(seconds, distDeg, '-', [seconds[i] for i in zeroCross], np.zeros(len(zeroCross)), 'o')
+plt.title('Quaternion angle over time with zerocross')
+plt.legend(['Angle', 'Zero'])
+plt.xlabel('time (s)')
+plt.ylabel('angle (deg)')
+plt.show()
+
+# Total Acceleration
+totalAcc = np.sqrt(np.square(acc_x[0:-1]) + np.square(acc_y[0:-1]))
+plt.plot(seconds, totalAcc, '-', [seconds[i] for i in zeroCross], np.zeros(len(zeroCross)), 'o')
+plt.title('Total Acceleration')
+plt.legend(['Acceleration', 'Zero'])
+plt.xlabel('time (s)')
+plt.ylabel('acceleration [m/s^2]')
+plt.show()
+
+# Quaternion amplitude
+posAmpQ = []
+negAmpQ = []
+tPos = []
+tNeg = []
+for i in range(0, len(zeroCross)-2):
+    if gyr_z[round((zeroCross[i+1]+zeroCross[i])/2)]>0:
+        i1 = zeroCross[i]
+        i2 = zeroCross[i+1]
+        posAmpQ.append(sum(distDeg[i1:i2]))
+        tPos.append(seconds[round((zeroCross[i+1]+zeroCross[i])/2)])
+    else:
+        i1 = zeroCross[i]
+        i2 = zeroCross[i + 1]
+        negAmpQ.append(sum(distDeg[i1:i2]))
+        tNeg.append(seconds[round((zeroCross[i + 1] + zeroCross[i]) / 2)])
+
+meanAmpQ = (np.array(posAmpQ)+np.array(negAmpQ))/2
+
+plt.bar(tPos, posAmpQ,width=0.3)
+plt.bar(tNeg, negAmpQ,width=0.3)
+plt.xlabel("Time (s)")
+plt.ylabel('Amplitude (deg)')
+plt.title("Amplitude per Kick: Up vs. Down")
+plt.legend("positive","negative")
+plt.show()
+
+tAvg = (np.array(tPos)+np.array(tNeg))/2
+plt.bar(tAvg, meanAmpQ)
+plt.xlabel("Time (s)")
+plt.ylabel('Amplitude (deg)')
+plt.title("Amplitude per Kick")
 plt.show()
